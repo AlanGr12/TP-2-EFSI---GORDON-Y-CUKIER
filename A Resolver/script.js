3c736bf8043e667b92a0a6bf67009547b3557acc
NICOLAS CUKIER <49006840@ort>
2026-03-25 08:45:34 -0300

    avanz tp

---
+++ A Resolver/script.js
@@ -0,0 +1,49 @@
+const input = document.getElementById("guessInput")
+const button = document.getElementById("guessBtn")
+const message = document.getElementById("message")
+const reset = document.getElementById("resetBtn")
+const attemptsText = document.getElementById("attempts")
+
+let secretNumber = Math.floor(Math.random() * 100) + 1
+let attempts = 0
+
+reset.addEventListener("click", resetearValores)
+
+button.addEventListener("click", checkGuess)
+
+let juegoTerm = false;
+
+function checkGuess(){
+
+  const guess = input.value
+
+  attempts = attempts + 1
+
+  attemptsText.innerText = attempts
+
+  if(guess == secretNumber){
+    message.innerText = "¡Ganaste!"
+    juegoTerm = true
+  }
+
+  else if(guess < secretNumber){
+    message.innerText = "El número es mayor"
+  }
+
+  else{
+    message.innerText = "El número es menor"
+  }
+
+}
+
+function resetearValores(){
+
+  secretNumber = Math.floor(Math.random() * 100) + 1
+  attempts = 0
+  juegoTerm = false
+
+  attempts = attempts + 1
+  
+  attemptsText.innerText = attempts
+
+}
